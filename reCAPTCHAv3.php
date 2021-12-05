f8614bfcacad1aee448ff8b7635781b4971cc075
joshaallen <jaalle10@dmacc.edu>
2021-12-05 16:06:48 -0600

    modified CSS for HTML Table

--- reCAPTCHAv3.php
+++ reCAPTCHAv3.php
@@ -1,35 +1,56 @@
 <?php
   //Building Data Model
+  $successMessage="";
   $errorMessage= "";
   $validFlag=false;
   $formSubmitFlag= false;
-  $name="";
+  $email="";
   $remoteIP="";
+  //functions
+  function emailValidate($value) {
+      $incorrectEmail = false;
+      if(strlen($value)!= 0) {
+          global $errorMessage;
+          $errorMessage = "Missing @";
+        if(str_contains($value, '@')) {
+          global $errorMessage; 
+          $errorMessage = "Missng . operator ";
+          if(str_contains($value, '.')) {
+            $incorrectEmail = true;
+            global $errorMessage;
+            $errorMessage = "";
+          }
+        }
+        
+      }
+      return $incorrectEmail;
+  }
+
   //validage whether form has been submitted
-  if($_SERVER['REQUEST_METHOD']=='POST' && !empty($_POST['fName'])) {
+  if($_SERVER['REQUEST_METHOD']=='POST' && emailValidate($_POST['email'])) {
     
     $validFlag = true;
-    $name = $_POST['fName'];
+    $email = $_POST['email'];
     $secret = "secret";
     $responseToken = $_POST["g-recaptcha-response"];
     $remoteIP =  getenv("REMOTE_ADDR");
     $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$responseToken&remoteip=$remoteIP";
     //the response is a JSON String
     $reCAPTCHAResponse = File_get_contents($url);
-    echo $reCAPTCHAResponse;
     //turned JSON String into PHP object for further processing
-    $reCAPTCHAResponse = json_decode($reCAPTCHAResponse);
+    $phpReCAPTCHAResponse = json_decode($reCAPTCHAResponse);
+     $arrayPHPReCAPTCHAResponse = (array)$phpReCAPTCHAResponse;
     //Validation to determine if reCAPTCHAResponse object success property is true or not
-     if($reCAPTCHAResponse->success==1 && $reCAPTCHAResponse->score >= 0.5 && $reCAPTCHAResponse->action == "submit" ) {
-       echo "Hello World";
+     if($phpReCAPTCHAResponse->success==1 && $phpReCAPTCHAResponse->score >= 0.5 && $phpReCAPTCHAResponse->action == "submit" ) {
+      $successMessage = "Congratulations you are Not a robot";
        
      }
      else {
-       echo "Didn't work";
+      $errorMessage =  "Something went wrong";
      }
   }
-  elseif($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST['fName'])){
-    $errorMessage = "Please fill in first name!";
+  elseif($_SERVER['REQUEST_METHOD']=='POST' && empty($_POST['email'])){
+    $errorMessage = "Please enter a email address!";
   }
 ?>
 <!DOCTYPE html>
@@ -45,26 +66,49 @@
   <script src="https://www.google.com/recaptcha/api.js"></script>
   <script>
    function onSubmit(token) {
-     document.getElementById("form1").submit();
+     document.getElementById("reCAPTCHAv3_form").submit();
    }
  </script>
- <link rel="stylesheet" href="reCAPTCHAv2.css">
+ <link rel="stylesheet" href="reCAPTCHAv3.css">
 </head>
 <body>
   <div class="container">
   <header>WDV351 Ecommerce Store</header>
   <main>
-  <form action="reCAPTCHAv3.php" method="post" id="form1">
-    <div>
-    <label for="fName">First Name:</label>
-    <input type="text" name="fName" id="fName" placeholder="Enter your name" required autofocus>
-    <?php echo $errorMessage;?>
+  <form action="reCAPTCHAv3.php" method="post" id="reCAPTCHAv3_form">
+    <div class="vertical--center">
+      <div>
+      <label for="email">Enter Email:</label>
+      </div>
+      <div>
+      <input type="email" name="email" id="email" placeholder="john@abc.com" size="30" maxlength="64" minlength="3" required autofocus>
+      </div>
+      <span class="error-message"><?php echo $errorMessage;?></span>
+      <span class="success-message"><?php echo $successMessage;?></span>
     </div>
-    <button class="g-recaptcha" data-sitekey="siteKey" data-callback="onSubmit" data-action="submit">Submit</button>
+    <button class="g-recaptcha" data-sitekey="siteKey" data-callback="onSubmit" data-action="submit">Signup</button>
+    <input type="reset" value="Reset">
+   
+    
   </form>
-  <?php if($validFlag) {echo "<div>Your are Verified!!!</div><div>Your Name is:$name</div><div>Your IP address is:$remoteIP</div>";header( "refresh:10; url=reCAPTCHAv3.php" );} ?>
+  <?php if($validFlag) {
+      $tablehead = [];
+      $tableData = [];
+      foreach($arrayPHPReCAPTCHAResponse as $key => $value) {
+          array_push($tablehead, $key);
+          array_push($tableData, $value);
+       }
+  ?>
+    <table>
+      <caption>reCAPTCHAv3 Results</caption>
+      <thead><tr><th><?php echo $tablehead[0];?></th><th><?php echo $tablehead[1];?></th><th><?php echo $tablehead[2];?></th><th><?php echo $tablehead[3];?></th></tr></thead>
+      <tbody><tr><td><?php echo $tableData[0];?></td><td><?php echo $tableData[1];?></td><td><?php echo $tableData[2];?></td><td><?php echo $tableData[3];?></td></tr></tbody>
+    </table>
+  <?php 
+  } 
+  ?>
   </main>
   <footer>WDV351 Ecommerce Store</footer>
-  </div>
+  </div><!--end of container-->
 </body>
 </html>
